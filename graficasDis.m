625c50594bb5b4ddb9f0368906b41e143f8e0a81
Tulio Carmona <48919666+TulioCarmona@users.noreply.github.com>
2019-05-28 21:47:03 -0500

    Add files via upload

---
+++ graficasDis.m
@@ -0,0 +1,33 @@
+function graficasDis()
+f1=openfig('graficaDis2.fig','reuse');
+ax1=gca;
+f2=openfig('graficaDis3.fig','reuse');
+ax2=gca;
+f3=openfig('graficaDis4.fig','reuse');
+ax3=gca;
+f4=openfig('graficaDis5.fig','reuse');
+ax4=gca;
+f5=openfig('graficaDis6.fig','reuse');
+ax5=gca;
+f=figure;
+s1=subplot(2,3,1);
+title('Respuesta al impulso')
+s2=subplot(2,3,2);
+title('Respuesta a entrada cero')
+s3=subplot(2,3,3);
+title('Respuesta a estado cero')
+s4=subplot(2,3,4);
+title('Respuesta total')
+s5=subplot(2,3,5);
+title('Respuesta al escalon')
+fig1=get(ax1,'children');
+fig2=get(ax2,'children');
+fig3=get(ax3,'children');
+fig4=get(ax4,'children');
+fig5=get(ax5,'children');
+copyobj(fig1,s1);
+copyobj(fig2,s2);
+copyobj(fig3,s3);
+copyobj(fig4,s4);
+copyobj(fig5,s5);
+end
